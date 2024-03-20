93f9e6c416ebb77d82832d69ba0e0094d7b6e85b
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-03-21 02:35:54 +0530

    added new models

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,41 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+import dotenv from "dotenv";
+
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { expressMiddleware } from "@apollo/server/express4";
 
 import mergedResolvers from "./resolvers/index.js";
 import mergedTypedefs from "./typeDefs/index.js";
 
+dotenv.config();
+
+const app = express();
+
+const httpServer = http.createServer(app);
 const server = new ApolloServer({
   typeDefs: mergedTypedefs,
   resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
-const { url } = await startStandaloneServer(server);
 
-console.log(`Server is ready at ${url}`);
+// Ensure we wait for our server to start
+await server.start();
+
+app.use(
+  "/",
+  cors(),
+  express.json(),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    context: async ({ req }) => req,
+  })
+);
+
+// Modified server startup
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+console.log(`🚀 Server ready at http://localhost:4000/`);
