e1fcb0b25444b1952dad0c968bac264af9731343
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-03-29 23:25:22 +0530

    added changes

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -28,8 +28,8 @@ const userResolver = {
         const salt = await bcrypt.genSalt(10);
         const hssPassword = await bcrypt.hash(password, salt);
 
-        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username${username}`;
         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username${username}`;
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username${username}`;
 
         const newUser = new User({
           username,
