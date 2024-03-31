5b109868d1311a8d5d2ecf10b578c202fc49421b
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-03-31 23:59:59 +0530

    added transaction resolvers

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,5 +1,11 @@
 const transactionResolver = {
-  Query: {},
+  Query: {
+    transactions: async (_, _, context) => {
+      try {
+        if (!context.getUser()) throw new Error("Unauthorised");
+      } catch (error) {}
+    },
+  },
   Mutation: {},
 };
 export default transactionResolver;
