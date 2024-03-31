b8b4afe24f30c81af878d25d935aa910882690e3
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-04-01 00:38:34 +0530

    added transaction resolver

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,9 +1,27 @@
+import Transaction from "../model/transaction.model.js";
 const transactionResolver = {
   Query: {
     transactions: async (_, _, context) => {
       try {
         if (!context.getUser()) throw new Error("Unauthorised");
-      } catch (error) {}
+        const userId = await context.getUser()._id;
+
+        const transactions = await Transaction.find({ userId });
+        return transactions;
+      } catch (error) {
+        console.error("Error getting transaction", error);
+        throw new Error("Error getting transaction");
+      }
+    },
+
+    transaction: async (_, { transactionId }) => {
+      try {
+        const transaction = await Transaction.findById(transactionId);
+        return transaction;
+      } catch (error) {
+        console.error("Error getting transaction", error);
+        throw new Error("Error getting transaction");
+      }
     },
   },
   Mutation: {},
