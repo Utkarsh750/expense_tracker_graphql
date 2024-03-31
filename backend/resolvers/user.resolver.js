1103da6c1359d5fd8848a8497098ec3268801193
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-03-31 23:32:04 +0530

    added login and logout reslver

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -47,7 +47,36 @@ const userResolver = {
         throw new Error(error.message || "Internal server error");
       }
     },
-    login: () => 
+    login: async (_, { input }, context) => {
+      try {
+        const { username, password } = input;
+        if (!username || !password) throw new Error("All fields are required");
+        const { user } = await context.authenticate("graphql-local", {
+          username,
+          password,
+        });
+
+        await context.login(user);
+        return user;
+      } catch (err) {
+        console.error("Error in login:", err);
+        throw new Error(err.message || "Internal server error");
+      }
+    },
+    logout: async (_, __, context) => {
+      try {
+        await context.logout();
+        context.req.session.destroy((err) => {
+          if (err) throw err;
+        });
+        context.res.clearCookie("connect.sid");
+
+        return { message: "Logged out successfully" };
+      } catch (err) {
+        console.error("Error in logout:", err);
+        throw new Error(err.message || "Internal server error");
+      }
+    },
   },
 };
 
