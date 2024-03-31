1fe0ded0f7f64fd1250ff9735acb2d6338f2c15c
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-03-31 23:40:19 +0530

    user resolvers added

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -4,11 +4,23 @@ import bcrypt from "bcryptjs";
 
 const userResolver = {
   Query: {
-    users: () => {
-      return users;
+    authUser: async (_, __, context) => {
+      try {
+        const user = await context.getUser();
+        return user;
+      } catch (err) {
+        console.error("Error in authUser: ", err);
+        throw new Error("Internal server error");
+      }
     },
-    user: (_, { userId }) => {
-      return users.find((user) => user._id === userId);
+    user: async (_, { userId }) => {
+      try {
+        const user = await User.findById(userId);
+        return user;
+      } catch (err) {
+        console.error("Error in user query:", err);
+        throw new Error(err.message || "Error getting user");
+      }
     },
   },
 
