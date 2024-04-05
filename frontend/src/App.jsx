fa0585210d7075ace2a9fc785033f86d1ab189a7
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-04-05 22:24:17 +0530

    added signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import TransactionPage from "./pages/TransactionPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import Header from "./component/ui/Header";
 function App() {
+  const authUser = true;
   return (
     <>
-      <div className=" text-red-700 bg-cyan-900">Hello</div>
+      {authUser && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 }
---
+++ frontend/src/pages/TransactionPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TransactionPage() {
+  return (
+    <div>Transaction</div>
+  )
+}
+
+export default TransactionPage
