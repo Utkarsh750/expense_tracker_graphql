62bb40c71e5f5f7f662f9597e60324a300bb56fb
Utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-04-11 23:47:00 +0530

    added cards history

--- frontend/src/component/Cards.jsx
+++ frontend/src/component/Cards.jsx
@@ -1,17 +1,26 @@
+import { GET_TRANSACTION } from "../graphql/queries/transaction.query";
 import Card from "./Card";
+import { useQuery } from "@apollo/client";
 
 const Cards = () => {
+  const { data, loading, error } = useQuery(GET_TRANSACTION);
+
+  if (loading) return <p>Loading...</p>;
+
+  console.log("data:", data);
   return (
     <div className="w-full px-10 min-h-[40vh]">
       <p className="text-5xl font-bold text-center my-10">History</p>
       <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-start mb-20">
-        <Card cardType={"saving"} />
-        <Card cardType={"expense"} />
-        <Card cardType={"investment"} />
-        <Card cardType={"investment"} />
-        <Card cardType={"saving"} />
-        <Card cardType={"expense"} />
+        {data.transactions.map((transaction) => (
+          <Card key={transaction._id} transaction={transaction} />
+        ))}
       </div>
+      {!loading && data?.transactions?.length === 0 && (
+        <p className="text-2xl font-bold text-center w-full">
+          No Transaction History found
+        </p>
+      )}
     </div>
   );
 };
